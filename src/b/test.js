70bfd97614bd3391aca1363f1ae28e7d724913b0
qixin <qixin@haoqix.com>
2018-05-11 14:48:26 +0800

    b/ test file update

--- src/b/test.js
+++ src/b/test.js
@@ -1,3 +1,20 @@
 /**
  * create test file at test branch
  */
+
+const p1 = new Promise((resolve, reject)=>{
+  resolve('hello')
+})
+
+const p2 = new Promise((resolve, reject)=>{
+  throw new Error('p2 error')
+})
+
+async function fun(){
+  try{
+    let ret = await Promise.all([p1, p2])
+    console.log('ret=>', ret)
+  }catch(e){
+    console.log('e =>', e)
+  }
+}
